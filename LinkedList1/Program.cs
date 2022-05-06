64a4ff3404e8bdeb7a3f1aecf6c67ae2b089703b
Vo Nguyen Tran Quoc <voquoc0103@gmail.com>
2022-05-06 13:43:28 +0700

    linked listed

---
+++ LinkedList1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace linkedlist
+{
+    class Node
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> numbers = new LinkedList<string>();
+
+            var num9 = numbers.AddFirst("9");
+            var num8 = numbers.AddLast("8");
+
+            var num3 = numbers.AddAfter(num9, "3");
+            var num4 = numbers.AddAfter(num3, "4");
+            var num7 = numbers.AddAfter(num4, "7");
+
+            foreach (var data in numbers)
+            {
+                Console.WriteLine(data);
+            }    
+        }
+    }
+}
